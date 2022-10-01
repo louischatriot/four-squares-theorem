d1125f0978987f375e20c44760171d4a17814432
Louis Chatriot <louis.chatriot@gmail.com>
2022-10-01 14:08:45 +0200

    Can do it for 4n + 1 and 4n + 3

--- main.py
+++ main.py
@@ -142,126 +142,155 @@ t = 821844181995567608861526435749234500883999
 
 
 
-# 1/0
+def check_answer(t, a, b, c, d):
+    print("RESULT")
+    print(a)
+    print(b)
+    print(c)
+    print(d)
 
+    res = t - a * a - b * b - c * c - d * d
+    print("CHECK")
+    print(res)
+
+    if res != 0:
+        print("FAIL")
+
+
+def four_square_brute_force(n):
+    a_max = math.isqrt(n)
+
+    for a in range(a_max, -1, -1):
+        if not three_squarable(n - a * a):
+            continue
+
+        b_max = math.isqrt(n - a * a)
+
+        for b in range(b_max, -1, -1):
+            c_max = math.isqrt(n - a * a - b * b)
+
+            for c in range(c_max, -1, -1):
+                sr = n - a * a - b * b - c * c
+                d = math.isqrt(sr)
+                if sr == d * d:
+                    return (a, b, c, d)
+
+
+def four_square_4n_2(t):
+    if t % 4 != 2:
+        raise BaseException("t should be of form 4k+2")
+
+    m = 1
+    p = 5
+
+    while m * m % p != p - 1:
+        a_max = math.isqrt(t)
+
+        for a in range(a_max, -1, -1):
+            if not three_squarable(t - a * a):
+                continue
+            else:
+                break
+
+        # The 1000 is arbitrary here but we want to avoid special case where b cannot be found because t - a * a is too small
+        if t - a * a < 1000:
+            return four_square_brute_force(t)
+
+        # Should actually have one odd and one even and test for it here but seems to work downstreams anyway
+        # a = randrange(1, math.isqrt(t))
+        b = randrange(1, math.isqrt(t - a * a))
+        p = t - a * a - b * b
+        r = (p - 1) // 4
+        x = randrange(1, p)
+
+        # Find our m
+        # See https://mathoverflow.net/questions/259152/efficient-method-to-write-number-as-a-sum-of-four-squares
+        e = r
+        y = 1
+        while e > 1:
+            if e % 2 == 0:
+                x = x * x % p
+                e = e // 2
+            else:
+                y = y * x % p
+                x = x * x % p
+                e = (e - 1) // 2
+
+        x = x * y % p
+        m = x
+
+    # Euclidian division until we reach sqrt(p)
+    # See https://en.wikipedia.org/wiki/Fermat%27s_theorem_on_sums_of_two_squares#Algorithm
+    c = p
+    d = m
+    r = p
+    while c > math.sqrt(p):
+        r = c - (c // d) * d
+        c = d
+        d = r
+
+    return (a, b, c, d)
+
+
+def four_square_4n_1_or_3(n):
+    if n % 4 != 1 and n % 4 != 3:
+        raise BaseException("n should be of form 4k+& or 4k+3")
+
+    l = four_square_4n_2(2 * n)
+
+    evens = []
+    odds = []
+    for i in l:
+        if i % 2 == 0:
+            evens.append(i)
+        else:
+            odds.append(i)
+
+    _a, _b = odds
+    _c, _d = evens
+    a = (_a + _b) // 2
+    b = (_a - _b) // 2
+    c = (_c + _d) // 2
+    d = abs(_c - _d) // 2
+
+    return a, b, c, d
 
 
 start = time.time()
 
-# n = 4k + 2
 # https://mathoverflow.net/questions/259152/efficient-method-to-write-number-as-a-sum-of-four-squares
-t = 821844181995567608861526435749234500883998
+
+# n = 4k + 2
+# t = 821844181995567608861526435749234500883998
+
+# 4k + 2
+# t = 42860344287450692837937001962400072422456192468221344297750015534814042044997444899727935152627834325103786916702125873007485811427692561743938310298794299215738271099296923941684298420249484567511816728612185899934327765069595070236662175784308251658284785910746168670641719326610497547348822672277518
+
+# 4k + 2
+# t = 171441377149802771351748007849600289689824769872885377191000062139256168179989779598911740610511337300415147666808503492029943245710770246975753241195177196862953084397187695766737193680997938270047266914448743599737311060278380280946648703137233006633139143642984674682566877306441990189395290689112074
+
+# print("=======================================")
+# print(t)
+
+# a, b, c, d = four_square_4n_2(t)
+
+# duration = time.time() - start
+# print(f"=====> {duration}")
 
 
-t = 4 * 2**1000 + 14
+start = time.time()
+
+# 4k + 1
+t = 685765508599211085406992031398401158759299079491541508764000248557024672719959118395646962442045349201660590667234013968119772982843080987903012964780708787451812337588750783066948774723991753080189067657794974398949244241113521123786594812548932026532556574571938698730267509225767960757581162756448297
 
 print("=======================================")
 print(t)
 
-
-m = 1
-p = 5
-
-while m * m % p != p - 1:
-    a_max = math.isqrt(t)
-
-    for a in range(a_max, -1, -1):
-        if not three_squarable(t - a * a):
-            continue
-        else:
-            break
-
-    # print("eeeeeeeeeeeeeee")
-    # print(t - a * a)
-
-    # Should actually have one odd and one even and test for it here but seems to work downstreams anyway
-    # a = randrange(1, math.isqrt(t))
-    b = randrange(1, math.isqrt(t - a * a))
-
-    p = t - a * a - b * b
-
-    r = (p - 1) // 4
-
-    x = randrange(1, p)
-
-    # print("-----")
-    # print(x)
-    # print(r)
-
-
-    e = r
-    y = 1
-
-    while e > 1:
-        if e % 2 == 0:
-            x = x * x % p
-            e = e // 2
-        else:
-            y = y * x % p
-            x = x * x % p
-            e = (e - 1) // 2
-
-    x = x * y % p
-
-    m = x
-
-
-print("------------------------")
-print(m)
-print(p)
-print(p - 1 - (m * m % p))
-
-
-
-
-# no = (m * m + 1) // p   # Exact division by p
-
-# print(no)
-# print(m * m + 1 - p * no)
-
-c = p
-d = m
-r = p
-while c > math.sqrt(p):
-    r = c - (c // d) * d
-    c = d
-    d = r
-
-print("EUCLIDE DONE")
-print(p - c * c - d * d)
-
-
-
-print("RESULT")
-print(t - a ** 2 - b ** 2 - c ** 2 - d ** 2)
-
-
-# print(m)
-
-
-# print("%%%%%%%")
-# print(m * m % p)
-# print(p - 1)
-
-
-
-
-# a, b, c, d = four_squares(t)
-
-print(a*a + b*b + c*c + d*d)
-
-
-
-
-
+a, b, c, d = four_square_4n_1_or_3(t)
+check_answer(t, a, b, c, d)
 
 duration = time.time() - start
 print(f"=====> {duration}")
-
-
-
-
-
 
 
 
